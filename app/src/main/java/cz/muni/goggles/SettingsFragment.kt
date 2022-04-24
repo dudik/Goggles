662a5f9484766f296e7ac7de3654a48f819d5b33
Richard Šoltis <rsoltis@behaimits.com>
2022-04-24 22:47:16 +0200

    SettingsFragment.kt add

---
+++ app/src/main/java/cz/muni/goggles/SettingsFragment.kt
@@ -0,0 +1,13 @@
+package cz.muni.goggles
+
+import android.os.Bundle
+import androidx.preference.PreferenceFragmentCompat
+
+
+class SettingsFragment : PreferenceFragmentCompat() {
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        addPreferencesFromResource(R.xml.preferences)
+    }
+
+}
