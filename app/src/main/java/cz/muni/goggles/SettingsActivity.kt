52f83f8bbca188aefc5bdafa25ccbf85f5cba9b2
Richard Šoltis <rsoltis@behaimits.com>
2022-04-24 22:47:02 +0200

    SettingsActivity.kt add

---
+++ app/src/main/java/cz/muni/goggles/SettingsActivity.kt
@@ -0,0 +1,19 @@
+package cz.muni.goggles
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import cz.muni.goggles.databinding.ActivitySettingsBinding
+
+class SettingsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySettingsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivitySettingsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportFragmentManager.beginTransaction().replace(R.id.fragmentContainerView, SettingsFragment()).commit()
+    }
+}
