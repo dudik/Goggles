cf77bf352cf8cd5ad068137a7a5d4dcca7e46bc1
Richard Šoltis <rsoltis@behaimits.com>
2022-05-04 08:18:29 +0200

    handle null in coverHorizontal

---
+++ app/src/main/java/cz/muni/goggles/GogApi.kt
@@ -0,0 +1,13 @@
+package cz.muni.goggles
+
+import cz.muni.goggles.classes.Game
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface GogApi {
+    @GET("products/{ids}")
+    fun getProductsByIds(
+        @Path("ids")ids: String
+    ): Call<Game>
+}
