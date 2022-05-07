35e99d778ff00802a9ce2099e12d8d948c4e26c2
duri488 <lazorik.juraj@gmail.com>
2022-05-07 17:31:07 +0200

    Fix condition for infinite scrolling

--- app/src/main/java/cz/muni/goggles/activities/MainActivity.kt
+++ app/src/main/java/cz/muni/goggles/activities/MainActivity.kt
@@ -102,7 +102,7 @@ class MainActivity : AppCompatActivity() {
             override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
                 super.onScrolled(recyclerView, dx, dy)
 
-                if (!recyclerView.canScrollVertically(1) && dy != 0 && (!following || !upcoming)) {
+                if (!recyclerView.canScrollVertically(1) && dy != 0 && (!following && !upcoming)) {
                     pageNumber++
                     refresh(pageNumber)
                 }
