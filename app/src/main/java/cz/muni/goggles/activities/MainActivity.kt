1d49677aa496f108bbb125b660820e911cdf3725
Samuel Dudík <xdudik@fi.muni.cz>
2022-05-07 15:14:50 +0200

    Fix infite scroll trigger when checking 'Following'

--- app/src/main/java/cz/muni/goggles/activities/MainActivity.kt
+++ app/src/main/java/cz/muni/goggles/activities/MainActivity.kt
@@ -103,8 +103,7 @@ class MainActivity : AppCompatActivity() {
             override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
                 super.onScrolled(recyclerView, dx, dy)
 
-                if (!recyclerView.canScrollVertically(1)) {
-                    println("SCROLL")
+                if (!recyclerView.canScrollVertically(1) && dy != 0) {
                     pageNumber++
                     refresh(pageNumber)
                 }
